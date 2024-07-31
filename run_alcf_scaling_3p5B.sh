7eb61cfb9a98d021f87fe64c7c97523dd4886392
Sam Foreman <saforem2@gmail.com>
2024-07-31 13:00:39 -0500

    Update `{run_alcf_scaling_3p5b.sh,ds_config_3p5B.json}`

--- run_alcf_scaling_3p5B.sh
+++ run_alcf_scaling_3p5B.sh
@@ -1,20 +1,44 @@
 #!/bin/bash --login
 
-cd "${PBS_O_WORKDIR}" || exit
-
 NOW="$(date "+%Y-%m-%d-%H%M%S")"
 
+cd "${PBS_O_WORKDIR}" || exit
 source "${PBS_O_WORKDIR}/deps/ezpz/src/ezpz/bin/utils.sh" || exit
 ezpz_setup_python || exit
 ezpz_setup_alcf "$@" || exit
 
-TP="${TP:-1}"
+PP=1
+TP=1
+NUM_LAYERS=16
+NUM_KV_HEADS=32
+NUM_ATTENTION_HEADS=32
+HIDDEN_SIZE=4096
+FFN_HIDDEN_SIZE=11008
+MAX_POSITION_EMBEDDINGS=4096
+ZERO_STAGE=1
+MICRO_BATCH=8
+SEQ_LEN=512
 
-CHECKPOINT_DIR="checkpoints/ds_stage2_nl16_hs4096_mb2_seq512_pp1_tp${TP}_bf16"
+CHECKPOINT_DIR="checkpoints/ds_stage${ZERO_STAGE}_nl${NUM_LAYERS}_hs${HIDDEN_SIZE}_mb${MICRO_BATCH}_seq${SEQ_LEN}_pp${PP}_tp${TP}_bf16"
 mkdir -p "${CHECKPOINT_DIR}"
 
 export WORLD_SIZE="${NGPUS}"
 run_cmd="${DIST_LAUNCH} python3 dpo_training.py \
+ --seq-length ${SEQ_LEN} \
+ --save ${CHECKPOINT_DIR} \
+ --load ${CHECKPOINT_DIR} \
+ --num-layers ${NUM_LAYERS} \
+ --hidden-size ${HIDDEN_SIZE} \
+ --micro-batch-size ${MICRO_BATCH} \
+ --tensor-model-parallel-size ${TP} \
+ --pipeline-model-parallel-size ${PP} \
+ --ffn-hidden-size ${FFN_HIDDEN_SIZE} \
+ --num-key-value-heads ${NUM_KV_HEADS} \
+ --num-attention-heads ${NUM_ATTENTION_HEADS} \
+ --max-position-embeddings ${MAX_POSITION_EMBEDDINGS} \
+ --zero-stage=${ZERO_STAGE} \
+ --attention-dropout 0 \
+ --hidden-dropout 0 \
  --bf16 \
  --split 100,0,0 \
  --log-interval 1  \
@@ -27,26 +51,14 @@ run_cmd="${DIST_LAUNCH} python3 dpo_training.py \
  --accumulate-allreduce-grads-in-fp32 \
  --use-checkpoint-opt_param-scheduler \
  --lr 5e-6 \
- --seq-length 512 \
- --save ${CHECKPOINT_DIR} \
- --load ${CHECKPOINT_DIR} \
- --num-layers 16 \
- --hidden-size 4096 \
  --train-iters 5000 \
  --eval-iters 10 \
  --distributed-backend ccl \
- --num-attention-heads 32 \
  --save-interval 5000 \
  --eval-interval 50000 \
- --max-position-embeddings 4096 \
- --micro-batch-size 2 \
  --data-file-list-p ALCF/data_p.txt \
  --data-file-list-u ALCF/data_u.txt \
- --tensor-model-parallel-size ${TP} \
- --pipeline-model-parallel-size 1 \
- --num-key-value-heads 32 \
  --data-cache-path ./index-cache \
- --ffn-hidden-size 11008 \
  --tokenizer-model ALCF/tokenizer.model \
  --no-query-key-layer-scaling \
  --use-rotary-position-embeddings \
@@ -54,16 +66,14 @@ run_cmd="${DIST_LAUNCH} python3 dpo_training.py \
  --swiglu \
  --normalization rmsnorm \
  --disable-bias-linear \
- --zero-stage=2 \
- --deepspeed_config=ds_config-gpt.json \
+ --deepspeed_config=ds_config_3p5B.json \
  --no-pipeline-parallel \
  --deepspeed \
  --optimizer adamw"
 
-
-LOGFILE="dpo_training_${NOW}.log"
+LOGFILE="dpo_training_3p5B_${NOW}.log"
 
 echo "Writing to ${LOGFILE}"
 echo "CHECKPOINT_DIR: ${CHECKPOINT_DIR}"
-echo "${run_cmd}" > "${LOGFILE}"
-eval "${run_cmd}" |& tee "${LOGFILE}"
+echo "${run_cmd}" >"${LOGFILE}"
+eval "${run_cmd}" |& tee -a "${LOGFILE}"
